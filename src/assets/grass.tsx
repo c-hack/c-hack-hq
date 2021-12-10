bfce1bfcacd93679d7a0343c566bd3c65d606570
surasto <ralf_stoffels@gmx.de>
2021-12-10 23:01:29 +0000

    Deploying to gh-pages from master @ eb6b10c801762e054a5cc01217804348a6110ae2 🚀

--- src/assets/grass.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="grass" tilewidth="32" tileheight="32" tilecount="70" columns="10">
- <image source="grass.png" trans="ff00ff" width="320" height="224"/>
-</tileset>
--- src/assets/tf_jungle_tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="tf_jungle_tileset" tilewidth="32" tileheight="32" tilecount="110" columns="11">
- <image source="tf_jungle_tileset.png" trans="ff00ff" width="352" height="336"/>
-</tileset>
