13e9b0e9f66dedc270594cbf797358376a11dbb2
surasto <ralf_stoffels@gmx.de>
2021-12-20 21:13:50 +0100

    animations

---
+++ src/assets/Mainframe.tsx
@@ -0,0 +1,47 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Mainframe" tilewidth="32" tileheight="32" tilecount="64" columns="8">
+ <grid orientation="orthogonal" width="8" height="32"/>
+ <image source="Mainframe.png" trans="ff00ff" width="256" height="256"/>
+ <tile id="1">
+  <animation>
+   <frame tileid="1" duration="200"/>
+   <frame tileid="4" duration="200"/>
+   <frame tileid="7" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="9">
+  <animation>
+   <frame tileid="9" duration="200"/>
+   <frame tileid="12" duration="200"/>
+   <frame tileid="15" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="17">
+  <animation>
+   <frame tileid="17" duration="200"/>
+   <frame tileid="20" duration="200"/>
+   <frame tileid="23" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="25">
+  <animation>
+   <frame tileid="25" duration="200"/>
+   <frame tileid="28" duration="200"/>
+   <frame tileid="31" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="33">
+  <animation>
+   <frame tileid="33" duration="200"/>
+   <frame tileid="36" duration="200"/>
+   <frame tileid="39" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="41">
+  <animation>
+   <frame tileid="41" duration="200"/>
+   <frame tileid="44" duration="200"/>
+   <frame tileid="47" duration="200"/>
+  </animation>
+ </tile>
+</tileset>
---
+++ src/assets/Whiteboard.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Whiteboard" tilewidth="32" tileheight="32" tilecount="14" columns="2">
+ <image source="Whiteboard.png" trans="ff00ff" width="64" height="224"/>
+</tileset>
